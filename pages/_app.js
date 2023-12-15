84b461ffbdd7bf0480a18601edd3def2f4df7939
Hendrik Pils <hendrik-pils@gmx.de>
2023-12-15 11:31:52 +0100

    finish challenge

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,36 @@
 import GlobalStyle from "../styles";
+import { SWRConfig } from "swr";
 
 export default function App({ Component, pageProps }) {
+  const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+
+  const fetcher = async (URL) => {
+    const res = await fetch(URL);
+
+    // If the status code is not in the range 200-299,
+    // we still try to parse and throw it.
+    if (!res.ok) {
+      const error = new Error("An error occurred while fetching the data.");
+      // Attach extra info to the error object.
+      error.info = await res.json();
+      error.status = res.status;
+      throw error;
+    }
+
+    return res.json();
+  };
+
   return (
     <>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <SWRConfig
+        value={{
+          fetcher,
+          refreshInterval: 5000,
+        }}
+      >
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </SWRConfig>
     </>
   );
 }
